07b37af44dcefcce9f5b4f021bbefe20ff0a45c3
snbroy <34596924+snbroy@users.noreply.github.com>
2025-01-05 21:22:51 +0530

    devsanjib frontend  code

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { login, register, updateUser } from "../controllers/authController.js";
-const router = express.Router()
-
-router.post('/login', login)
-router.post('/register', register)
-router.post('/updateUser', updateUser)
-
-export default router;
