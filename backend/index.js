3dcd50803b49138e5b036339d5dfb91e596f4683
snbroy <34596924+snbroy@users.noreply.github.com>
2025-01-05 21:40:15 +0530

    devsanjib frontend  code

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const PORT = 8080
 
 import connectToDatabase from "./db/db.js"
 
-import authRouter from "./routes/index.js"
+import authRouter from "./routes/auth.js"
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors())
@@ -18,7 +18,7 @@ app.get('/', (req,res)=>{
     })
 })
 
-app.use("/api/", authRouter)
+app.use("/api/auth", authRouter)
 
 app.listen(PORT, ()=>{
     console.log(`Successfully Listening on ${PORT}`)
